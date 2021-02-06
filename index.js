116c8b1a521e1079529e7010f11d03952b18471a
yawar <yawar.integril@gmail.com>
2021-02-06 14:51:27 +0500

    socketIO_Express_JsonServer

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
+ 
+// // server.listen(process.env.PORT);
 var jsonServer = require('json-server');
+const app = require('express')();
 
-// Returns an Express server
-var server = jsonServer.create();
+app.use('/api', jsonServer.defaults(), jsonServer.router('db.json'));
+// app.listen(3000);
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(jsonServer.defaults());
+ 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Add custom routes
-// server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
 
-// Returns an Express router
-var router = jsonServer.router('db.json');
+app.get('/soc', (req, res) => {
+    //   res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/Views/index.html');
+});
 
-server.use(router);
 
-server.listen(process.env.PORT);
 
-// server.listen(3000);
+io.on('connection', (socket) => {
+    console.log("loggeduser => " +  socket.handshake.query.loggeduser);
+    socket.on('chat message', (msg) => {
+        // create or join room
+        // socket.join('some room');
+        // io.sockets.connected[socketID].join(roomName);
+
+
+        // console.log('message: ' + msg);
+        
+        // This will emit the event to all connected sockets
+        // io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+        
+        // send to specific SocketId
+        // io.to(socket.id).emit('chat message', 'for your eyes only');
+        // io.sockets.connected[socketid].emit('message', 'for your eyes only');
+
+
+        // send to everyone
+        io.emit('chat message',socket.id + ':' + msg); 
+
+        // send to everyone except sender
+        // socket.broadcast.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+// http.listen(process.env.PORT, () => {
+http.listen("3000", () => {
+    // console.log('listening on *:' + process.env.PORT);
+});
